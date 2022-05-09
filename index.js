95eaff161703205c418548afd87685440f781442
Volodymyr Makhobei <bratikua@gmail.com>
2022-05-09 20:49:08 +0300

    Project done

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 //Foresight project
+let container = {
+    when: ['Tomorrow', 'The day after tomorrow', 'On the weekend', 'Next week', 'Next month', 'Next year'],
+    cond: ['you will have', 'will be', 'you will got', 'you will lose', 'you will not have'],
+    what: ['big luck', 'small problems', 'tragedy', 'fantastic progress in your work', 'super profit', 'some shity thing']
+};
+
+function foresight (obj){
+    let phrase = [];
+    //chosing random word or phrase out of each arr in obj
+    for(let key in obj){
+        phrase.push(obj[key][Math.floor(Math.random()*obj[key].length)])
+    }
+    console.log(`${phrase.join(' ')}.`);
+}
+
+foresight(container);
